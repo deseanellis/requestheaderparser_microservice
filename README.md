4dc15d006cfa7513f4615ca95002549bf9eac1f8
System <none>
2017-09-29 22:55:12 +0000

    🍛🤑 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# requestheaderparser_microservice
-fCC Request Header Parser Microservice
+Free Code Camp | Request Header Parser Microservice
+=========================
+
+This microservice returns information from the requester gathered from the request header of the HTTP packet sent to the server from your client device. The return format is JSON.
+
+The service will return the IP address, default language, browser and operating system from your request.
+
+How to Use
+-------------
+Access the API via /api/whoami/
+
+`https://massive-receipt.glitch.me/api/whoami/`
+
+Developed by DeSean Ellis - Free Code Camp
---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var parse = require('user-agent-parser');
+var useragent = require('useragent');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/whoami/", function (req, res) {
+  var agent = useragent.parse(req.headers["user-agent"]);
+  res.json({ipaddress: req.headers["x-forwarded-for"].split(",")[0], language: req.headers["accept-language"].split(",")[0], browser: agent.toAgent(), software: agent.os.toString()});
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
